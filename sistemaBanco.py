61fa5b62350b84b1f7a2e9e24b11efb442e04e4c
Lucas1362 <jamersontenorio88@gmail.com>
2024-05-31 17:45:05 -0300

    saque completo

--- sistemaBanco.py
+++ sistemaBanco.py
@@ -1,9 +1,36 @@
+#interface basica 
 saldo = 1000
 
 print("olá bem-vindo ao app Brazexcco")
 print(f"Seu saldo disponivel é R$ {saldo:.2f} ")
-print(" (1) Extrato (2) Saque (3) Depositar ")
-input("selecione qual função você deseja utilizar: ")
+print(" (1) Extrato (2) Deposito (3) Saque ")
+escolha = int(input("selecione qual função você deseja utilizar: "))
 
+while escolha < 1 or escolha > 3:
+    print("Esta escolhar é invalida. Por favor digite um numero valido")
+    escolha = int(input("selecione qual função você deseja utilizar: "))
 
-opcoes = 
+# Interface de Saque
+
+if escolha ==  3:
+    confirmacao = input("Você escolheu a opção saque, deseja continuar? ")
+    
+    #loop caso o cliente tenha errado a escolha
+    
+    while confirmacao == "não":
+        print(" (1) Extrato (2) Deposito (3) Saque ")
+        escolha = int(input("selecione qual função você deseja utilizar: "))
+        confirmacao = input("Você escolheu a opção saque, deseja continuar? ")
+    print(f"O seu saldo disponivel: R$ {saldo:.2f}")
+    valor = int(input("Quanto deseja retirar: "))
+    #loop caso o saque seja invalido
+    
+    while valor > saldo:
+        print("você não possui este saldo disponivel :/.")
+        valor = int(input("por favor informe quanto você deseja retirar: "))
+         
+    
+    calculo_v_retirado = saldo - valor
+    
+    print(f"Você retirou R$ {valor:.2f} e seu saldo atual é R$ {calculo_v_retirado:.2f}")
+        
