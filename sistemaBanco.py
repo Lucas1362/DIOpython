d3dcee700dc80be5ac7521cfbc150538fef114f4
Lucas1362 <jamersontenorio88@gmail.com>
2024-05-31 16:49:22 -0300

    primeiraParte

--- sistemaBanco.py
+++ sistemaBanco.py
@@ -1,1 +1,9 @@
-aaa
+saldo = 1000
+
+print("olá bem-vindo ao app Brazexcco")
+print(f"Seu saldo disponivel é R$ {saldo:.2f} ")
+print(" (1) Extrato (2) Saque (3) Depositar ")
+input("selecione qual função você deseja utilizar: ")
+
+
+opcoes = 
