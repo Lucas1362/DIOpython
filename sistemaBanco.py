654b09e93926ac8dc8e3cc68bc52c3957f9f173b
Lucas1362 <jamersontenorio88@gmail.com>
2024-05-31 22:59:59 -0300

    mexer nos aspectos esteticos do codigo

--- sistemaBanco.py
+++ sistemaBanco.py
@@ -1,13 +1,17 @@
 #interface basica 
+
 saldo = 1000
 
 print("olá bem-vindo ao app Brazexcco")
 print(f"Seu saldo disponivel é R$ {saldo:.2f} ")
 print(" (1) Extrato (2) Deposito (3) Saque ")
+
 escolha = int(input("selecione qual função você deseja utilizar: "))
 
 while escolha < 1 or escolha > 3:
+    
     print("Esta escolhar é invalida. Por favor digite um numero valido")
+    
     escolha = int(input("selecione qual função você deseja utilizar: "))
 
 # Interface de Saque
@@ -19,8 +23,10 @@ if escolha ==  3:
     
     while confirmacao == "não":
         print(" (1) Extrato (2) Deposito (3) Saque ")
+        
         escolha = int(input("selecione qual função você deseja utilizar: "))
         confirmacao = input("Você escolheu a opção saque, deseja continuar? ")
+    
     print(f"O seu saldo disponivel: R$ {saldo:.2f}")
     valor = int(input("Quanto deseja retirar: "))
     
@@ -30,7 +36,6 @@ if escolha ==  3:
         print("você não possui este saldo disponivel :/.")
         valor = int(input("por favor informe quanto você deseja retirar: "))
          
-    
     novo_saldo = saldo - valor
     
     print(f"Você retirou R$ {valor:.2f} e seu saldo atual é R$ {novo_saldo:.2f}")
@@ -38,14 +43,19 @@ if escolha ==  3:
     
 if escolha == 2: 
     confirmacao = input("Você escolheu a opção deposito, deseja continuar? ")
+    
     while confirmacao == "não":
         print(" (1) Extrato (2) Deposito (3) Saque ")
+        
         escolha = int(input("selecione qual função você deseja utilizar: "))
         confirmacao = input("Você escolheu a opção deposito, deseja continuar? ")
+    
     print(f"Saldo disponivel: R${saldo:.2f}")
     valor = float(input("quanto deseja depositar: "))
     
-    while valor < 0:
+    #o codigo não ira aceitar valores negativos e o proprio zero
+    while valor <= 0:
+        
         print("Valor invalido. Por favor informe um valor positivo.")
         valor = float(input("quanto deseja depositar: "))
     
@@ -56,6 +66,6 @@ if escolha == 2:
     
 # escolha para voltar a interface inicial
 
-escolha_retorno = input("Deseja realizar outra ação(sim/não)? ".lower())      
+
     
         
