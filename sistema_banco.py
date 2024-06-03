2e2b7a01c2d8ad9b7a277536ec218056275c4cf6
Lucas1362 <jamersontenorio88@gmail.com>
2024-06-02 22:24:16 -0300

    correção de semantica e adição de extrato

--- sistema_banco.py
+++ sistema_banco.py
@@ -13,12 +13,12 @@ while escolha < 1 or escolha > 3:
 
 # Interface de Saque
 if escolha == 3:
-    confirmacao = input("Você escolheu a opção saque, deseja continuar? ").lower()
+    confirmacao = input("Você escolheu a opção saque, deseja continuar? ")
     
-    while confirmacao != "sim":
+    while confirmacao.lower() == "não":
         print("(1) Extrato (2) Depósito (3) Saque")
         escolha = int(input("Selecione qual função você deseja utilizar: "))
-        confirmacao = input("Você escolheu a opção saque, deseja continuar? ").lower()
+        confirmacao = input("Você escolheu a opção saque, deseja continuar? ")
     
     print(f"O seu saldo disponível: R$ {saldo:.2f}")
     valor = float(input("Quanto deseja retirar: "))
@@ -32,13 +32,13 @@ if escolha == 3:
     print(f"Você retirou R$ {valor:.2f} e seu saldo atual é R$ {saldo:.2f}")
 
 # Interface de Depósito
-elif escolha == 2: 
-    confirmacao = input("Você escolheu a opção depósito, deseja continuar? ").lower()
+if escolha == 2: 
+    confirmacao = input("Você escolheu a opção depósito, deseja continuar? ")
     
-    while confirmacao != "sim":
+    while confirmacao.lower() == "não":
         print("(1) Extrato (2) Depósito (3) Saque")
         escolha = int(input("Selecione qual função você deseja utilizar: "))
-        confirmacao = input("Você escolheu a opção depósito, deseja continuar? ").lower()
+        confirmacao = input("Você escolheu a opção depósito, deseja continuar? ")
     
     print(f"Saldo disponível: R$ {saldo:.2f}")
     valor = float(input("Quanto deseja depositar: "))
@@ -52,7 +52,7 @@ elif escolha == 2:
     print(f"Você depositou R$ {valor:.2f} e seu saldo atual é R$ {saldo:.2f}")
 
 # Interface de Extrato
-elif escolha == 1:
+if escolha == 1:
     print("Bem-vindo ao seu extrato")
     if extrato_produtos:
         for transacao in extrato_produtos:
